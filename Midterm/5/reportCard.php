f6d451df50718ca8ba8f8332418bfe6fe9addf8d
Andrés Bretón <bretonics@users.noreply.github.com>
2019-02-27 19:09:28 -0500

    Q5

---
+++ Midterm/5/reportCard.php
@@ -0,0 +1,10 @@
+<?php
+
+$reportCard = array("sheehan" => array("601" => "90"), 
+                    "smitty" => array("601" => "82"),
+                    "Andres" => array("602" => "98")
+);
+
+echo "The grade for sheehan in 601 is " . $reportCard["sheehan"]["601"]. "<br>"; 
+echo "The grade for smitty in 601 is " . $reportCard["smitty"]["601"]. "<br>";
+?>
